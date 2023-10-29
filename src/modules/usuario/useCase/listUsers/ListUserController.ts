ba5845e1856c889df756c5619d570e5f1b268b7d
Tayssa Victoria <tayssa@ipeweb.com.br>
2023-10-29 10:16:01 -0300

    adicionando modal inserir conta

--- src/modules/usuario/useCase/listUsers/ListUserController.ts
+++ src/modules/usuario/useCase/listUsers/ListUserController.ts
@@ -9,7 +9,7 @@ class ListUserController {
     try {
       const users = await listUserUseCase.execute();
 
-      return response.status(201).send(users);
+      return response.status(200).send(users);
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
