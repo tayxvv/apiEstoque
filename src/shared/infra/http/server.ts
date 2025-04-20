10f7d9c00b21362ccb1f85a90fadd6bcab6f1b6e
tayssa <tayssavictoria2004@gmail.com>
2025-04-20 09:54:17 -0300

    categorias

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,3 +7,7 @@ import { app } from './app';
 const PORT = process.env.PORT || 3334;
 
 app.listen(PORT, () => console.log("Server is running"));
+
+app.get('/', (req, res) => {
+    res.send('API rodando');
+  });
