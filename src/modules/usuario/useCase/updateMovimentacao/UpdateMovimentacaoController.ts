098afee6f87b25c1e75e9667e00741a0a64a738c
tayssa <tayssavictoria2004@gmail.com>
2025-05-24 21:57:20 -0300

    arrumando editar movimentações

--- src/modules/usuario/useCase/updateMovimentacao/UpdateMovimentacaoController.ts
+++ src/modules/usuario/useCase/updateMovimentacao/UpdateMovimentacaoController.ts
@@ -5,13 +5,13 @@ import { container } from "tsyringe";
 class UpdateMovimentacaoController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
-    const { observacao, quantidade, data, id_produto, id_tipo_movimentacao } = request.body;
+    const { observacao, quantidade, data } = request.body;
     const updateMovimentacaoUseCase = container.resolve(UpdateMovimentacaoUseCase);
 
     try {
-      await updateMovimentacaoUseCase.execute(id, { observacao, quantidade, data, id_produto, id_tipo_movimentacao });
+      await updateMovimentacaoUseCase.execute(id, { observacao, quantidade, data });
 
-      return response.status(201).send({ observacao, quantidade, data, id_produto, id_tipo_movimentacao });
+      return response.status(201).send({ observacao, quantidade, data });
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
